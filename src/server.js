6636990d348f26e8d48646e1ec3c88acc3c8adaf
margl13 <m.glubochenko@gmail.com>
2022-09-01 05:11:24 +0200

    heroku deploy updates

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ app.use(express.static(__dirname + '/dist/<ngchat>'));
 app.get('/*', function (req,res) {
   res.sendFile(path.join(__dirname + '/dist/<ngchat>/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
 
