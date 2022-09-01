ab26ade1a73ebbd4b73bd0eb4b4cc3b9d216d4f5
margl13 <m.glubochenko@gmail.com>
2022-09-01 05:54:04 +0200

    heroku deploy updates

--- src/server.js
+++ src/server.js
@@ -5,5 +5,6 @@ app.use(express.static(__dirname + '/dist/<ngchat>'));
 app.get('/*', function (req,res) {
   res.sendFile(path.join(__dirname + '/dist/<ngchat>/index.html'));
 });
-app.listen(process.env.PORT || 3000);
 
+const port = process.env.PORT || 8000;
+app.listen(port, () => {console.log("App is running on port " + port)});
