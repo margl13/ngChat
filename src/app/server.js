0a32c77924ebf4162308fc7e4e586379a80960c9
margl13 <m.glubochenko@gmail.com>
2022-09-01 13:43:31 +0200

    heroku deploy updates

--- src/app/server.js
+++ src/app/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<ngchat>'));
+app.use(express.static(__dirname + '/dist/ngchat'));
 app.get('/*', function (req,res) {
-  res.sendFile(path.join(__dirname + '/dist/<ngchat>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ngchat/index.html'));
 });
 
 const port = process.env.PORT || 5000;
